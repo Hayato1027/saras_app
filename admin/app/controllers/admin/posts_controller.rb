59eb58d0993b032bc40e5f3a74d25883afe74e7b
小磯颯斗 <hayato@koisosatsutonoMacBook-Pro-3.local>
2018-11-10 02:53:09 +0900

    finished mountable_engine

---
+++ admin/app/controllers/admin/posts_controller.rb
@@ -0,0 +1,47 @@
+module Admin
+  class PostsController < Admin::ApplicationController
+    def index
+      @posts = Post.all
+    end
+
+    def show
+      post
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.new(posts_params)
+      if @post.save
+        redirect_to posts_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      post
+    end
+
+    def update
+      post.update(posts_params)
+    end
+
+    def destroy
+      post.destroy
+      redirect_to posts_path
+    end
+
+    private
+
+    def post
+      @post ||= Post.find(params[:id])
+    end
+
+    def posts_params
+      params.require(:post).permit(:title, :content)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  
+  resources :posts, only: [:index, :show]
+
   if defined? Admin::Engine
-    mount BlogEngine::Engine, at: '/admin'
+    mount Admin::Engine, at: '/admin'
   end
 end
